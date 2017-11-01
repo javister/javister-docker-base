5aabac795184a908b0eab77e5496855556d7e5c7
Victor Verbitsky <vektor@krista.ru>
2017-11-01 16:57:25 +0300

    Обновление скрипта сборки. По умолчанию  происходит только локальная сборка. Для того, чтобы отправить собраный образ на bintray.org нужно указать ключ -r

--- build.sh
+++ build.sh
@@ -1,21 +1,55 @@
 #!/bin/bash -e
 
-VERSION=1.0
-DATE=$(date +"%Y-%m-%d")
+function build() {
+    local release
+    release="dry"
 
-IMAGE_TAG="javister-docker-docker.bintray.io/javister/javister-docker-base"
-PROXY_ARGS="--build-arg http_proxy=${http_proxy} \
-            --build-arg no_proxy=${no_proxy}"
-docker pull centos:7
+    while getopts ":rh" opt; do
+        case $opt in
+            r)
+                release="release"
+                ;;
+            h)
+                cat <<EOF
+usage: build [OPTION]... [-- [docker build opts]]
+  -h        show this help.
+  -r        pull base image and push resulting image too.
+EOF
+                return 0;
+                ;;
+            :)
+                echo "$0: option requires an argument -- '$OPTARG'" 1>&2
+                return 1
+                ;;
+            *)
+                echo "$0: invalid option -- '$OPTARG'" 1>&2
+                return 1
+                ;;
+        esac
+    done
+    shift $((OPTIND-1))
 
-docker build \
-    --tag ${IMAGE_TAG}:latest \
-    --tag ${IMAGE_TAG}:${VERSION} \
-    --tag ${IMAGE_TAG}:${VERSION}-${DATE} \
-    ${PROXY_ARGS} \
-    $@ \
-    .
+    VERSION=1.0
+    DATE=$(date +"%Y-%m-%d")
 
-docker push ${IMAGE_TAG}:latest
-docker push ${IMAGE_TAG}:${VERSION}
-docker push ${IMAGE_TAG}:${VERSION}-${DATE}
+    IMAGE_TAG="javister-docker-docker.bintray.io/javister/javister-docker-base"
+    PROXY_ARGS="--build-arg http_proxy=${http_proxy} \
+                --build-arg no_proxy=${no_proxy}"
+    [ "${release}" == "release" ] && docker pull centos:7
+
+    docker build \
+        --tag ${IMAGE_TAG}:latest \
+        --tag ${IMAGE_TAG}:${VERSION} \
+        --tag ${IMAGE_TAG}:${VERSION}-${DATE} \
+        ${PROXY_ARGS} \
+        $@ \
+        .
+
+    [ "${release}" == "release" ] && docker push ${IMAGE_TAG}:latest
+    [ "${release}" == "release" ] && docker push ${IMAGE_TAG}:${VERSION}
+    [ "${release}" == "release" ] && docker push ${IMAGE_TAG}:${VERSION}-${DATE}
+}
+
+trap "exit 1" INT TERM QUIT
+
+build "$@"
